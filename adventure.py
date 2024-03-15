aee8b3fccd9ec38b9370ff231c086ce1b5b88f55
itsjenm <jmartinez414@gmail.com>
2024-03-15 14:01:24 -0500

    this is a new commit

--- adventure.py
+++ adventure.py
@@ -1,5 +1,12 @@
 def new_adventure():
     print("Ahoy, I am embarking in a new adventure")
 
+a = '''this is a string containing
+multiple lines of text
+and this is the second line of text'''
+
+print(a)
+
 if __name__ == "__main__":
     new_adventure()
+    
